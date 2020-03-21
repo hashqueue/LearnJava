5bd0637f88cf45dcf014aa587b757938259a3666
wenbin <1912315910@qq.com>
2020-03-21 13:40:38 +0800

    add Java语言概述与开发环境

---
+++ src/com/abc/part_one/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.abc.part_one;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        //换行打印
+        System.out.println("Hello World!");
+        //不换行打印
+        System.out.print("Hello World!");
+        System.out.print("Hello World!");
+    }
+}
