18ae010ddf41dae4706bcba95d3130de982d8785
wenbin <1912315910@qq.com>
2020-03-22 00:40:42 +0800

    add 数据类型和运算符

---
+++ src/com/abc/part_two/DataTypesAndOperators.java
@@ -0,0 +1,22 @@
+package com.abc.part_two;
+
+public class DataTypesAndOperators {
+    public static void main(String[] args) {
+        /*
+        这是多行注释
+        */
+        /**
+         这是文档注释
+         这是文档注释
+         这是文档注释
+         这是文档注释
+         */
+        //这是单行注释
+        int age = 21;
+        String name = "小华";
+        String hello = "hello "
+                + name;
+        System.out.println("HelloJava");
+//        System.out.println("这行代码被注释了，程序运行时这行代码不会被执行");
+    }
+}
