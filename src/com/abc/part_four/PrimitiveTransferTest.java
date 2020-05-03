0641e2aa66e4256d65f3139b6d2eb2cb71fa1801
wenbin <1912315910@qq.com>
2020-05-04 00:13:41 +0800

    add 方法详解

---
+++ src/com/abc/part_four/PrimitiveTransferTest.java
@@ -0,0 +1,34 @@
+package com.abc.part_four;
+
+/**
+ * @Auther: ABC
+ * @Date: 2020/5/3 12:27
+ * @Description: 基本类型的值传递代码示例：验证值传递就是将`实际参数值的副本(复制品)`传入方法内，而参数本身不会受到任何影响。
+ */
+public class PrimitiveTransferTest {
+    /*
+
+     */
+    public static void swap(int a, int b) {
+        int tmp = a;
+        a = b;
+        b = tmp;
+        System.out.println("a和b交换后的值为：" + "a = " + a + ", b = " + b);
+    }
+
+    public static void main(String[] args) {
+        /**
+         * 当程序执行swap()方法时,系统进入swap()方法，并将main()法中的a,b变量作为参数值传入swap()方法,
+         * 传入swap()方法的只是a,b的副本，而不是a,b本身。
+         */
+        int a = 3;
+        int b = 4;
+        swap(3, 4);
+        System.out.println("交换结束后的a, b值为：" + "a = " + a + ", b = " + b);
+    }
+    /**
+     * 输出：
+     * a和b交换后的值为：a = 4, b = 3
+     * 交换结束后的a, b值为：a = 3, b = 4
+     */
+}
---
+++ src/com/abc/part_four/Recursive.java
@@ -0,0 +1,67 @@
+package com.abc.part_four;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @Auther: ABC
+ * @Date: 2020/5/3 22:29
+ * @Description:
+ */
+public class Recursive {
+    /**
+     * 计算一个整数的阶乘,如果用字母n来代表一个整数，阶乘代表着所有`小于或等于n`且大于0的整数的乘积。
+     * 阶乘通常简写成 n!
+     * 例如: 5! = 1 * 2 * 3 * 4 * 5 = 120
+     *
+     * @param n
+     * @return 计算结果
+     */
+    public static int fn(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            //方法中调用它自身,就是方法递归
+            return n * fn(n - 1);
+        }
+    }
+
+    /**
+     * 递归函数实现斐波那契数列
+     * 斐波那契数列：[1,1,2,3,5,8,13,21,34....],第一个数是1，后面的数等于前面两个数相加的结果
+     * 打印1-50内的斐波拉切数列
+     *
+     * @param m
+     * @return 计算结果
+     */
+    public static int fn1(int m) {
+        if (m == 1 || m == 2) {
+            return 1;
+        } else {
+            return fn1(m - 1) + fn1(m - 2);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int result = fn(5);
+        System.out.println("5! = " + result);//5! = 120
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入需要打印斐波拉契数列中的数字个数：");
+        int numCount = scanner.nextInt();
+        List<Integer> list=new ArrayList<Integer>();
+        for (int i = 1; i <= numCount; i++){
+            list.add(fn1(i));
+        }
+        System.out.print("斐波拉契数列中前" + numCount + "个数字为：" + list);
+        /**
+         * 输出：
+         * 5! = 120
+         * 请输入需要打印斐波拉契数列中的数字个数：
+         * 10
+         * 斐波拉契数列中前10个数字为：[1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+         */
+    }
+}
