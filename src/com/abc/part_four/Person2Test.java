4ff9a2b8a1b943d3c991de27db3492bd9dd731e7
passerby223 <1912315910@qq.com>
2020-05-08 16:58:34 +0800

    add Java面向对象之封装

---
+++ src/com/abc/part_four/Person2Test.java
@@ -0,0 +1,12 @@
+package com.abc.part_four;
+
+public class Person2Test {
+
+    public static void main(String[] args) {
+        Person2 person2 = new Person2();
+        person2.setName("小花花");
+        person2.setAge(21);
+        System.out.println("我叫" + person2.getName() + ", 今年" + person2.getAge() + "岁了!");//我叫小花花, 今年21岁了!
+    }
+
+}
