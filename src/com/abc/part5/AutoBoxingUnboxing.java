d4549b94fff59a3c5070ce458b57682140dbecd4
passerby223 <1912315910@qq.com>
2020-05-18 20:05:07 +0800

    add Java8增强的包装类

---
+++ src/com/abc/part5/AutoBoxingUnboxing.java
@@ -0,0 +1,16 @@
+package com.abc.part5;
+
+/**
+ * @author mi
+ */
+public class AutoBoxingUnboxing {
+    public static void main(String[] args) {
+        //直接把一个int基本类型变量赋值给integer类型的变量
+        Integer intVar1 = 5;
+        //直接把一个int基本类型变量赋值给Object类型的变量
+        Object intVar2 = 6;
+        //直接把一个boolean基本类型变量赋值给Object类型的变量
+        Object boolVar3 = true;
+        int aa = intVar1;
+    }
+}
